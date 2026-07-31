406ca09020377c0b2bd93460755839169256a011
YALLICO OWNER <yallico2024@gmail.com>
2026-07-30 20:41:29 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ global.botNumber = ""
 
 global.owner = [
   ["51927174369", "Carlos 👑", true],
-  ["56927308426", "Canada", true],
-  ["56949824726", "XxX", true]
+  ["528621029907", "Canada", true],
+  ["573003304931", "XxX", true]
 ]
 
 global.botname = '𝚂𝚑𝚒𝚣𝚞𝚔𝚊'
