969330521f8203203059aa0e42c9d4e4d2710038
YALLICO OWNER <yallico2024@gmail.com>
2026-08-01 07:18:56 -0500

    Update _on.js

--- plugins/_on.js
+++ plugins/_on.js
@@ -1,12 +1,24 @@
-let handler = async (m, { conn, command, isAdmin }) => {
-    if (!isAdmin) return m.reply('❌ Solo admins')
+let handler = async (m, { conn, command }) => {
+    console.log('ENTRE AL PLUGIN') // Si no sale esto, el plugin no carga
     
-    global.db.data.chats[m.chat].bannedGrupo = command === 'offbot'
-    await global.db.write()
+    let chat = global.db.data.chats[m.chat]
+    if (!chat) global.db.data.chats[m.chat] = chat = {}
     
-    m.reply(command === 'offbot' ? '🔴 Apagado' : '🟢 Prendido')
+    if (command == 'offbot') {
+        chat.bannedGrupo = true
+        await global.db.write()
+        await m.reply('🔴 Apagado')
+    }
+    
+    if (command == 'onbot') {
+        chat.bannedGrupo = false
+        await global.db.write()
+        await m.reply('🟢 Prendido')
+    }
 }
-handler.command = ['offbot','onbot']
+
+handler.command = ['offbot', 'onbot']
 handler.admin = true
 handler.group = true
+
 export default handler
