58fd3083444fbdc12ac31a5f1c27bdbed51d440e
YALLICO OWNER <yallico2024@gmail.com>
2026-08-01 07:40:56 -0500

    Eliminar _off.js

--- plugins/_off.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m) => {
-    global.db.data.chats[m.chat].bannedGrupo = true
-    m.reply('🔴 Bot Apagado en este grupo')
-}
-handler.command = ['offbot']
-handler.admin = true
-handler.group = true
-export default handler
