cfa8c6b287f88100e5f0e67420aefe613417fe41
YALLICO OWNER <yallico2024@gmail.com>
2026-08-01 07:36:40 -0500

    Update _on.js

--- plugins/_on.js
+++ plugins/_on.js
@@ -1,24 +1,8 @@
-let handler = async (m, { conn, command }) => {
-    console.log('ENTRE AL PLUGIN') // Si no sale esto, el plugin no carga
-    
-    let chat = global.db.data.chats[m.chat]
-    if (!chat) global.db.data.chats[m.chat] = chat = {}
-    
-    if (command == 'offbot') {
-        chat.bannedGrupo = true
-        await global.db.write()
-        await m.reply('🔴 Apagado')
-    }
-    
-    if (command == 'onbot') {
-        chat.bannedGrupo = false
-        await global.db.write()
-        await m.reply('🟢 Prendido')
-    }
+let handler = async (m) => {
+    global.db.data.chats[m.chat].bannedGrupo = false
+    m.reply('🟢 Bot Prendido en este grupo')
 }
-
-handler.command = ['offbot', 'onbot']
+handler.command = ['onbot']
 handler.admin = true
 handler.group = true
-
 export default handler
